2be52b9fdafb29789fd8709ec597c4e9b1e8c714
Takunoji <tak4545@outlook.jp>
2019-07-15 11:57:54 +0900

    Add Request to Server and Iinsert to DB.
    画面から入力したデータをDBに登録するためのPHPにデータを送信する処理を追加

--- SampleMap.js
+++ SampleMap.js
@@ -1,4 +1,76 @@
 /**************************************************
+ *          GeoLocationAPIスクリプト          *
+ **************************************************/
+// グローバル変数
+var id, target;
+
+function getCurrentPos() {
+    if (navigator.geolocation == false) {
+        console.log("GEO Locaion is unable.");
+        return;
+    }
+    // オプション・オブジェクト
+    var optionObj = {
+    	"enableHighAccuracy": false ,
+    	"timeout": 5000 ,
+    	"maximumAge": 0 ,
+    };
+    target = {
+        latitude: 0,
+        longitufe: 0
+    };
+    // Navigator.geolocation
+    id = navigator.geolocation.watchPosition(
+        successCurrentPos // 成功時のコールバック
+        , errorCurrentPos // 失敗時のコールバック
+        , optionObj); // オプション設定
+
+}
+
+/**
+ * GeoLocationの成功時のコールバック
+ * 参照：https://qiita.com/akkey2475/items/29a5e5ab0e0ca04f4169
+ */
+function successCurrentPos(position) {
+    var crd  = position.coords;
+    // マップの位置情報オブジェクト
+    var mapLatLng = new google.maps.LatLng(crd.latitude, crd.longitude);
+
+    if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
+      console.log('Congratulations, you reached the target');
+      navigator.geolocation.clearWatch(id);
+    }
+    // マーカーの追加
+    var current = new google.maps.Marker({
+        map: map,
+        position: mapLatLng,
+        icon: './mapImg/you.png'
+    });
+    console.log("test");
+}
+/**
+ * GeoLocationのエラー時のコールバック
+ * 参照：https://qiita.com/akkey2475/items/29a5e5ab0e0ca04f4169
+ */
+function errorCurrentPos(error) {
+    // エラーコード(error.code)の番号
+    // 0:UNKNOWN_ERROR				原因不明のエラー
+    // 1:PERMISSION_DENIED			利用者が位置情報の取得を許可しなかった
+    // 2:POSITION_UNAVAILABLE		電波状況などで位置情報が取得できなかった
+    // 3:TIMEOUT					位置情報の取得に時間がかかり過ぎた…
+
+    // エラー番号に対応したメッセージ
+    var errorInfo = [
+        "原因不明のエラーが発生しました…。" ,
+        "位置情報の取得が許可されませんでした…。" ,
+        "電波状況などで位置情報が取得できませんでした…。" ,
+        "位置情報の取得に時間がかかり過ぎてタイムアウトしました…。"
+    ];
+
+    alert(errorInfo[error.code]);
+    id  = null;
+}
+/**************************************************
  *          ライブハウス照会機能実装スクリプト          *
  **************************************************/
 /** 登録時に選択する位置情報のマーカー */
@@ -10,7 +82,9 @@ var xhr;
 
 /** 画面の初期化 */
 function loadView() {
-    // XMLHttpRequest
+    // GeoLocation API
+    getCurrentPos();
+    // XMLHttpRequestの生成
     xhr = createXmlHttpRequest();
 
     sideWin = document.getElementById('sideWin');
@@ -26,9 +100,7 @@ function loadView() {
     };
 
     // Load Overlay
-    var areaData = getSampleData();
-    console.log(areaData);
-
+    var areaData = dstMapData();
 }
 
 /** 画面の開閉 */
@@ -42,28 +114,12 @@ function sideWinHandle() {
     sideWin.hidden = !sideWin.hidden;
 }
 
-/** サンプルデータ(オーバーレイ)の取得 */
-function getSampleData() {
-    var dataArray = [];
-    dataArray.push(createMapData('Zepp Sapporo'
-                    ,'〒064-0809 北海道札幌市中央区南９条西４丁目４'
-                    ,'https://www.zepp.co.jp/hall/sapporo/'
-                    ,43.049468
-                    ,141.353902));
-
-    dataArray.push(createMapData('STUDIO810'
-                    ,'〒064-0808 北海道札幌市中央区南８条西４丁目４２２'
-                    ,'http://www.f-a-l.co.jp/810/'
-                    ,43.049805
-                    ,141.353793));
-
-    dataArray.push(createMapData('LIVE HOUSE SPIRITUAL LOUNGE'
-                    ,'〒060-0062 札幌市中央区南2西4丁目10番地　Large Countory bld. BF'
-                    ,'http://www.spirituallounge.jp/'
-                    ,43.057624
-                    ,141.352180));
-
-    return dataArray;
+/**
+1. PHPでマップデータ(オーバーレイ)の取得
+2. 取得したデータをHTMLのDIVタグに出力する
+ */
+function dstMapData() {
+ //
 }
 
 /** サンプルデータ(JSON)作成 */
@@ -83,8 +139,13 @@ function viewLatLng(mapEvent, map) {
     if (selectedMarker != undefined || selectedMarker != null) {
         selectedMarker.setMap(null);
     }
-    document.getElementById('latValue').textContent = mapEvent.latLng.lat();
-    document.getElementById('lngValue').textContent = mapEvent.latLng.lng();
+    // 表示部分に設定
+    document.getElementById('latText').textContent = mapEvent.latLng.lat();
+    document.getElementById('lngText').textContent = mapEvent.latLng.lng();
+    // 送信部分に設定
+    document.getElementById('latValue').value = mapEvent.latLng.lat();
+    document.getElementById('lngValue').value = mapEvent.latLng.lng();
+
     selectedMarker = new google.maps.Marker(selectedOverlayOpt(mapEvent));
     selectedMarker.setMap(map);
 }
@@ -103,7 +164,7 @@ function selectedOverlayOpt(mapEvent) {
 
 /** セットしたデータをPHPに送信する */
 function postData() {
-    ajaxGet();
+    ajaxPost();
 }
 
 /** XMLHttpRequest作成 */
@@ -125,14 +186,38 @@ function recieveResponse() {
 }
 /** Ajax送信処理 */
 function ajaxPost() {
+    xhr.open('POST', 'https://zenryokuservice.com/tools/maps/InsertMapInfo.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;  charset=UTF-8"');
+    xhr.onreadystatechange = recieveResponse;
+    // POSTデータを設定する
+    var data = createJSON();
+    try {
+        xhr.send(data);
+    } catch(e) {
+        console.log(e);
+    }
 }
 
+// application/x-www-form-urlencoded
+// application/json
 /** Ajax送信 */
 function ajaxGet() {
     console.log(xhr);
     xhr.open('GET', 'https://zenryokuservice.com/tools/maps/InsertMapInfo.php?param=test', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onreadystatechange = recieveResponse;
 
     xhr.send();
+}
+
+/** POST送信するデータ(JSON)を作成する */
+function createJSON() {
+    var form = document.getElementById("mapData");
+    var data = new FormData();
+    data.appen("name", form.name.value);
+    data.appen("url", form.url.value);
+    data.appen("aFile", form.imageFile.files[0]);
+    data.append("lat", form.lat.value);
+    data.append("lng", form.lng.value);
+    return data;
 }
